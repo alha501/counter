15a9a502a34f969915294d006c1eca443b728be9
alha501 <113909973+alha501@users.noreply.github.com>
2022-10-31 09:23:24 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
-let counter = 9
-basic.forever(function () {
     for (let index = 0; index <= 4; index++) {
         led.plot(index, 0)
         basic.pause(200)
     }
+})
+input.onButtonPressed(Button.B, function () {
     for (let index = 0; index <= 4; index++) {
         led.unplot(4 - index, 0)
         basic.pause(200)
     }
+})
+let counter = 9
+basic.forever(function () {
+	
 })
